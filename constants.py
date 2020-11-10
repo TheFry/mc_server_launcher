9bd9657975ba05e37c28a811b0abdd0802db2fab
Luke Matusiak <me@lukematusiak.com>
2020-11-09 19:31:46 -0800

    Added check_dir and download_jar functions
    check_dir() runs from main() before menu(). It checks that the directory
    $HOME/.mc_server_launcher exists and creates it if necessary. It then checks
    if the server.jar file exists. If not, it calls download_jar.
    
    download_jar(path) takes a Path object pointing to where the server.jar should
    be saved. It grabs a json file from mojang that includes the latest
    version and the download link associated.

--- constants.py
+++ constants.py
@@ -1,14 +1,25 @@
-SERVER_DIR = "/home/luke/mc_server_launcher"
 VERSION = "1.0"
+TITLE = "Minecraft Server Manager " + VERSION
+VERSIONS_URL = "https://launchermeta.mojang.com/mc/game/version_manifest.json"
+
+SERVER_DIR = ".mc_server_launcher/"
+SCREEN_NAME = "mc_server"
+
 OPTIONS = ["launch: Start a server",
            "kill: Stop a server",
            "help: More info about this program",
-           "exit: Close this manager. Server will continue to run."
-          ]
+           "exit: Close this manager. Server will continue to run." ]
+
 COMMANDS = ["launch", "kill", "help"]
 
 OPTIONS_STYLE = "\n   "
 PROMPT = "> "
+
 EXIT = "Exiting server manager"
 BAD_C = "Command not found"
 CLEAR_C = "clear"
+MEM = "2G"
+LAUNCH_C = ["java",
+            "-Xms" + MEM,
+            "-Xmx" + MEM,
+            "-jar", "server.jar"]
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import subprocess
 import signal
 import time
+import requests
+import json
+import pprint
+from pathlib import Path
 from constants import *
 
-
+HOME = str(Path.home()) + "/"
 
   
 def help():
@@ -16,25 +20,28 @@ def kill():
 
 def launch():
   # to do
-  print("launch")
+  subprocess.run(["ls", "-1"])
+  input()
 
 
 def menu():
   userin = ""
   num_commands = len(COMMANDS) - 1
   funcs = globals()
+  index = 0
 
   subprocess.run(CLEAR_C)
   while(userin != "exit"):
-    print("Minecraft Server Manager", VERSION)
+    print(TITLE)
     print("Options:", *OPTIONS, sep = OPTIONS_STYLE)
     userin = input(PROMPT)
     if userin == "exit": exit(0)
     for command in COMMANDS:
-      if(userin == command):
+      index = command.index
+      if userin == command:
         funcs[command]()
         continue
-      elif(command.index == num_commands):
+      elif index == num_commands:
         print(userin + ":", BAD_C)
         subprocess.run(CLEAR_C)
     
@@ -45,10 +52,51 @@ def sig_handler(signum, frame):
     print("\n", EXIT)
     exit(sigint)  
 
+# Download the latest jar from mojang
+def download_jar(path):
+  mc_versions = json.loads(requests.get(VERSIONS_URL).text)
+  latest = mc_versions["latest"]["release"]
+
+  jar_url = None
+  f = None
+  r = None
+
+  for version in mc_versions["versions"]:
+    if version["id"] == latest:
+      jar_url = version["url"]
+  
+  if(jar_url == None):
+    print("Error, could not download server.jar")
+    exit(1)
+
+  r = requests.get(jar_url)
+  f = open(path, "wb")
+  f.write(r.content)
+  r.close()
+  f.close()
+
+
+# Check if install directory exists and create if needed
+# Download latest jar if no jar is found. This is not an update function
+def check_dir():
+  path_str = HOME + SERVER_DIR
+  print(path_str)
+  p = Path(path_str)
+
+  if not p.exists():
+    print("Creating directory" + path_str)
+    p.mkdir()
+
+  p = Path(path_str + "server.jar")
+  if not p.exists():
+    print("Downloading latest server.jar")
+    download_jar(p)
+
 
 def main():
   signal.signal(signal.SIGINT, sig_handler)
-  menu()
+  check_dir()
+  # menu()
 
 if __name__ == "__main__":
   main()
