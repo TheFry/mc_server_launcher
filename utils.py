2659a2dc1525e734d9c0e0755c8e2a9dc6f5efb4
Luke Matusiak <me@lukematusiak.com>
2020-11-16 17:30:51 -0800

    added safe_mkdir and safe_json_update
    safe_json_update requires python 3.9 or greater

--- utils.py
+++ utils.py
@@ -20,11 +20,42 @@ def yes_no(question: str = "") -> int:
     else: print("Invalid response")
 
 
+def safe_mkdir(p: Path) -> int:
+  try:
+    p.mkdir()
+  except Exception as err:
+    print("Directory already exists: {0}".format(err))
+    return 1
+  except Exception as err:
+    print("Could not create directory: {0}".format(err))
+    return 2
+  return 0
+  
+
+def safe_json_update(p: Path, updates: dict) -> int:
+  current: dict = safe_json_loads(p)
+  if current is None:
+    print("merge_dictionary() error")
+    return 1
+  
+  try: 
+    current |= updates
+  except Exception as err:
+    print("merge_dictionary(): {0}".format(err))
+    return 1
+
+  if safe_json_dumps(p, current):
+    print("merge_dictionary() error")
+    return 1
+  return 0
+  
+
+  
 def safe_json_dumps(p: Path, d: dict) -> int:
   json_indent = 4
   try:
     p.write_text(json.dumps(d, indent = json_indent))
-  except TypeError as err:
+  except Exception as err:
     print("Could not dump json data to {0}: {1}".format(p, err))
     return 1
   return 0
